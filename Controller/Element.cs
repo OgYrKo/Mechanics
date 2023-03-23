0bec887c9fbd4013b917574974c78167730a686d
Maksym <mgalchinskyi@gmail.com>
2023-03-23 18:09:44 +0200

    All works before change rotation

---
+++ Controller/Element.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.DirectX;
+using Microsoft.DirectX.Direct3D;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    internal class Element
+    {
+        private Device device;
+        private Mesh cylinder;
+        private Material cylinderMaterial;
+        private Element prevElement;
+        private Vector3 startPoint;
+        private Vector3 endPoint;
+        Vector3 elementVector;
+
+        private const float RADIUS = 0.05f;
+
+        public Element(Device device, Vector3 startPoint,Vector3 endPoint)
+        {
+            SetElement(device, startPoint, endPoint);
+            this.prevElement = null;
+        }
+        public Element(Device device, Vector3 startPoint, Vector3 endPoint,Element prevElement)
+        {
+            SetElement(device, startPoint, endPoint);
+            this.prevElement = prevElement;
+        }
+
+        private void SetElement(Device device, Vector3 startPoint, Vector3 endPoint)
+        {
+            this.device = device;
+            this.startPoint = startPoint;
+            this.endPoint = endPoint;
+            SetCylinder();
+            SetCylinderMaterial();
+            SetDevice();
+        }
+
+        private void SetCylinder()
+        {
+            elementVector = endPoint - startPoint;
+            cylinder = Mesh.Cylinder(device, RADIUS, RADIUS, elementVector.Length(), 16, 1);
+        }
+
+        private void SetCylinderMaterial()
+        {
+            cylinderMaterial = new Material();
+            cylinderMaterial.Diffuse = Color.Yellow;
+            cylinderMaterial.Specular = Color.White;
+        }
+
+        private void SetDevice()
+        {
+            device.Material = cylinderMaterial;
+            //device.Transform.World *=Matrix.Translation(new Vector3(0,5f,0));
+
+            //device.Transform.World = Matrix.RotationX((float)Math.Acos((double)elementVector.Y / (double)elementVector.Length()))
+            //                          * Matrix.RotationY((float)Math.Acos((double)elementVector.Z / (double)elementVector.Length()))
+            //                          * Matrix.RotationZ((float)Math.Acos((double)elementVector.X / (double)elementVector.Length()))
+            //                          * Matrix.Translation(new Vector3(0,5f,0));
+                                      //* Matrix.Translation(new Vector3(elementVector.X/2, elementVector.Y / 2, elementVector.Z / 2));
+        }
+         
+        public void DrawElement()
+        {
+            SetDevice();
+            cylinder.DrawSubset(0);
+        }
+
+    }
+}
--- Controller/Form1.cs
+++ Controller/Form1.cs
@@ -11,19 +11,87 @@ namespace Controller
     public partial class Form1 : Form
     {
         Device d3d;
-        Mesh cylinder;
-        Material cylinder_material;
-        bool fl = true;
+        Element elementCylinder;
 
         public Form1()
         {
             InitializeComponent();
             d3d = null;
-            SetStyle(ControlStyles.ResizeRedraw, true);
-            cylinder = null;
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            StartSetup();
+            SetupProekcii();
+            SetElements();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Vector3 translationVector = new Vector3();
+            float rotationX = 0, rotationY = 0;
+            // Перемещение камеры вперед и назад
+            switch (e.KeyCode) 
+            {
+                case Keys.Up:
+                    translationVector.Y = -0.1f;
+                    break;
+                case Keys.Down:
+                    translationVector.Y = 0.1f;
+                    break;
+                case Keys.Left:
+                    translationVector.X = 0.1f;
+                    break;
+                case Keys.Right:
+                    translationVector.X = -0.1f;
+                    break;
+                case Keys.PageUp:
+                    translationVector.Z = 0.1f;
+                    break;
+                case Keys.PageDown:
+                    translationVector.Z = -0.1f;
+                    break;
+                case Keys.A:
+                    rotationY = 0.1f;
+                    break;
+                case Keys.D:
+                    rotationY = -0.1f;
+                    break;
+                case Keys.W:
+                    rotationX = 0.1f;
+                    break;
+                case Keys.S:
+                    rotationX = -0.1f;
+                    break;
+            }
+            d3d.Transform.View *= Matrix.Translation(translationVector);
+            d3d.Transform.View *= Matrix.RotationX(rotationX);
+            d3d.Transform.View *= Matrix.RotationY(rotationY);
+            
+            Invalidate(); // Перерисовываем окно
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            d3d.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Azure, 1.0f, 0);
+            d3d.BeginScene();
+            DrawElements();
+            DrawAxis();
+            d3d.EndScene();
+            d3d.Present();//Показываем содержимое дублирующего буфера
+        }
+
+        private void DrawElements()
+        {
+            elementCylinder.DrawElement();
+        }
+
+        private void SetElements()
+        {
+            elementCylinder = new Element(d3d, new Vector3(0, 0, 5f), new Vector3(0, 1, 5f));
+        }
+
+        private void StartSetup()
         {
             try
             {
@@ -45,92 +113,31 @@ namespace Controller
                 MessageBox.Show(this, exc.Message, "Ошибка инициализации");
                 Close(); // Закрываем окно
             }
-            cylinder = Mesh.Cylinder(d3d, 0.5f, 0.5f, 1.0f, 16, 1);
-            cylinder_material = new Material();
-            cylinder_material.Diffuse = Color.Yellow;
-            cylinder_material.Specular = Color.White;
-        }
-
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-            // Перемещение камеры вперед и назад
-            if (e.KeyCode == Keys.Up)
-            {
-                d3d.Transform.View *= Matrix.Translation(new Vector3(0, 0.1f, 0));
-            }
-            else if (e.KeyCode == Keys.Down)
-            {
-                d3d.Transform.View *= Matrix.Translation(new Vector3(0, -0.1f, 0));
-            }
-
-            // Перемещение камеры вправо и влево
-            if (e.KeyCode == Keys.Right)
-            {
-                d3d.Transform.View *= Matrix.Translation(new Vector3(0.1f, 0, 0));
-            }
-            else if (e.KeyCode == Keys.Left)
-            {
-                d3d.Transform.View *= Matrix.Translation(new Vector3(-0.1f, 0, 0));
-            }
-
-            // Перемещение камеры вверх и вниз
-            if (e.KeyCode == Keys.PageUp)
-            {
-                d3d.Transform.View *= Matrix.Translation(new Vector3(0, 0, 0.1f));
-            }
-            else if (e.KeyCode == Keys.PageDown)
-            {
-                d3d.Transform.View *= Matrix.Translation(new Vector3(0, 0, -0.1f));
-            }
-
-            // Поворот камеры вокруг оси Y
-            if (e.KeyCode == Keys.A)
-            {
-                d3d.Transform.View *= Matrix.RotationY(0.1f);
-            }
-            else if (e.KeyCode == Keys.D)
-            {
-                d3d.Transform.View *= Matrix.RotationY(-0.1f);
-            }
-
-            // Поворот камеры вокруг оси X
-            if (e.KeyCode == Keys.W)
-            {
-                d3d.Transform.View *= Matrix.RotationX(0.1f);
-            }
-            else if (e.KeyCode == Keys.S)
-            {
-                d3d.Transform.View *= Matrix.RotationX(-0.1f);
-            }
-
-            Invalidate(); // Перерисовываем окно
-        }
-
-        private void Form1_Paint(object sender, PaintEventArgs e)
-        {
-            d3d.Clear(ClearFlags.Target | ClearFlags.ZBuffer, Color.Azure, 1.0f, 0);
-            d3d.BeginScene();
-            SetupProekcii();
-            DrawAxis();
-            d3d.Material = cylinder_material;
-            d3d.Transform.World =
-            Matrix.RotationX((float)Math.PI / 6) *
-            Matrix.Translation(new Vector3(0, 0, 5f));
-            cylinder.DrawSubset(0);
-            d3d.EndScene();
-            //Показываем содержимое дублирующего буфера
-            d3d.Present();
         }
 
         private void SetupProekcii()
+        {
+            SetupLight();
+            SetupCamera();
+        }
+
+        private void SetupLight()
         {
             // Устанавливаем параметры источника освещения
             // Устанавливаем параметры источника освещения
             d3d.Lights[0].Enabled = true; // Включаем нулевой источник освещения
             d3d.Lights[0].Diffuse = Color.White; // Цвет источника освещения
-            d3d.Lights[0].Position = new Vector3(0, 0, 0); // Задаем координаты
-            d3d.Transform.Projection = Matrix.PerspectiveFovLH((float)Math.PI / 4,
-            this.Width / this.Height, 1.0f, 50.0f);
+            d3d.Lights[0].Direction = new Vector3();
+            d3d.Lights[0].Position = new Vector3(0, 0, -10); // Задаем координаты
+        }
+
+        private void SetupCamera()
+        {
+            d3d.Transform.Projection = Matrix.PerspectiveFovLH(
+                (float)Math.PI / 4,//высота видимости
+                this.Width / this.Height, //ширина видимости
+                1.0f, 50.0f);//передний и задний план
+            d3d.Transform.View = Matrix.LookAtLH(new Vector3(0, 0, -5f), new Vector3(), new Vector3(0, 1, 0));
         }
 
         private void DrawAxis()
@@ -139,7 +146,7 @@ namespace Controller
             d3d.Material = new Material();
 
             // Создаем вершины сетки координат
-            CustomVertex.PositionColored[] gridVertices = new CustomVertex.PositionColored[6];
+            CustomVertex.PositionColored[] gridVertices = new CustomVertex.PositionColored[10];
 
             int axisLen = 10;
 
@@ -153,11 +160,18 @@ namespace Controller
             gridVertices[4] = new CustomVertex.PositionColored(new Vector3(0, 0, axisLen), Color.White.ToArgb());
             gridVertices[5] = new CustomVertex.PositionColored(new Vector3(0, 0, -axisLen), Color.White.ToArgb());
 
-            
+
+            //axis y
+            //gridVertices[6] = new CustomVertex.PositionColored(new Vector3(0, 1, -axisLen), Color.White.ToArgb());
+            //gridVertices[7] = new CustomVertex.PositionColored(new Vector3(0, 1, axisLen), Color.White.ToArgb());
+            //axis x
+            gridVertices[8] = new CustomVertex.PositionColored(new Vector3(1, 0, axisLen), Color.White.ToArgb());
+            gridVertices[9] = new CustomVertex.PositionColored(new Vector3(1, 0, -axisLen), Color.White.ToArgb());
+
+
             // Рисуем вершины сетки координат
             d3d.VertexFormat = CustomVertex.PositionColored.Format;
-            d3d.DrawUserPrimitives(PrimitiveType.LineList, 6, gridVertices);
+            d3d.DrawUserPrimitives(PrimitiveType.LineList, 10, gridVertices);
         }
-
     }
 }
