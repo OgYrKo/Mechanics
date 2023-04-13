67018515facc21ac675bd43ef270f7ea1e8014c4
Maksym <mgalchinskyi@gmail.com>
2023-04-13 14:55:25 +0300

    Linguistic complete

---
+++ Lab4/Linguistics.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Linguistics
+    {
+        List<string> firstOrder;
+        List<string> secondOrder;
+        List<string> negative;
+        string mainWord;
+
+        public Linguistics(List<string> firstOrder, List<string> secondOrder, List<string> negative, string mainWord)
+        {
+            this.firstOrder = firstOrder;
+            this.secondOrder = secondOrder;
+            this.negative = negative;
+            this.mainWord = mainWord;
+        }
+
+        public List<string> FirstOrder()
+        {
+            List<string>strings = new List<string>();
+            for(int i = 0; i < firstOrder.Count; i++)
+            {
+                strings.Add(firstOrder[i]+" "+mainWord);
+            }
+            return strings;
+        }
+
+        public List<string> FirstAndSecondOrder()
+        {
+            List<string> firstStrings = FirstOrder();
+            List<string> strings = new List<string>();
+
+            for (int j = 0; j < secondOrder.Count; j++)
+            {
+                strings.Add(secondOrder[j] + " " + firstStrings[0]);
+                if(firstStrings.Count - 1!=0)
+                strings.Add(secondOrder[j] + " " + firstStrings[firstStrings.Count-1]);
+            }
+
+            return strings;
+        }
+
+
+        public List<string> All()
+        {
+            List<string> secondStrings = FirstAndSecondOrder();
+            List<string> strings = new List<string>();
+
+            for (int i = 0; i < secondStrings.Count; i++)
+            {
+                for (int j = 0; j < negative.Count; j++)
+                {
+                    strings.Add(negative[j] + " " + secondStrings[i]);
+                }
+            }
+            return strings;
+        }
+
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> input1 = new List<string>() {"важкий","легкий" };
+            List<string> input2 = new List<string>() { "дуже", "занадто", "досить" };
+            List<string> input3 = new List<string>() { "не" };
+            string mainWord = "предмет";
+
+            Linguistics linguistics = new Linguistics(input1, input2, input3,mainWord);
+            List<string> r1 = linguistics.FirstOrder();
+            List<string> r2 = linguistics.FirstAndSecondOrder();
+            List<string> r3 = linguistics.All();
+            Print(r1);
+            Print(r2);
+            Print(r3);
+        }
+
+        static void Print(List<string> strings)
+        {
+            foreach(string s in strings)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
